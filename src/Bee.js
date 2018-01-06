d85589e80fd920435b18dae273c9acd37d1f17b1
shawnhr <shawnk723@live.com>
2018-01-06 15:03:12 -0800

    Completed bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
+//const Grub = require('./Grub')
 var Bee = function() {
+ Grub.call(this);
+ this.age = 5;
+ this.color = 'yellow';
+ this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+console.log(Bee.prototype)
+Bee.prototype.constructor = Bee
+
+var bee = new Bee();
+//console.log(bee.constructor);
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.canFly = true;
+  this.treasureChest = [];
+  this.job = 'find pollen';
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+}
